b0e764d142ed8e071d5fb917f130a31683fe02c5
Lucas Cramer <lucas.cramer@commit.dev>
2021-06-19 14:35:35 -0700

    Fix typo in base URL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const sendAndReceiveRelatedThoughts = (thought) => {
-    const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : 'https://thought-bubbles-hop-backend-api.onboarding.dev/';
+    const baseUrl = process.env.NODE_ENV === 'development' ? 'http://localhost:8080/api' : 'https://thought-bubbles-hop-backend-api.onboarding.dev';
     console.log(`Environment: ${process.env.NODE_ENV}`);
 
     return axios({
